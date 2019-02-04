97be8b31ed57912fca9febedf656024cbaf4d04a
Bobby Roe <bobbyroe@gmail.com>
2019-02-04 14:24:13 -0800

    add foods and some things to letter.json

--- src/FoodQuad.js
+++ src/FoodQuad.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import "./Quadrant.css";
+import data from './letters.json';
 
 class FoodQuad extends Component {
 
-  
+    constructor() {
+        super();
+        this.state = {
+            food: "GERP",
+            hex: "#000000"
+        }
+    }
+
+    componentDidMount() {
+        const obj = data[this.props.letter];
+        this.setState({
+            food: (obj !== undefined) ? obj.food : "NOPE",
+            hex: (obj !== undefined) ? obj.hex : "#404040"
+        })
+    }
+
     render() {
         return (
             <div className="quadrant" id="food-quad">
-                TEXT GOES HERE
+                {this.state.food}
         </div>
         );
     }
